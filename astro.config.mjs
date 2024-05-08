67eff668add0b6e3f1f769ad161237390fc2ad6c
yikZero <i@yikzero.com>
2024-05-08 12:25:56 +0800

    perf: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rosure.yikzero.com/',
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap()]
 });
