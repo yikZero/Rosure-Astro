ce16aeb73d02763ca56e46fe7d85151c29322d06
yikZero <i@yikzero.com>
2024-05-08 12:37:25 +0800

    build: Yield to preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rosure.yikzero.com/',
   integrations: [tailwind({
     applyBaseStyles: false
-  }), sitemap()]
+  }), sitemap(), preact()]
 });
