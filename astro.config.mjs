65220ae92584cab2b0f705ee48197de10a907d75
yikZero <i@yikzero.com>
2024-05-08 09:50:42 +0800

    perf: adjust tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import preact from '@astrojs/preact';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rosure.yikzero.com/',
-  integrations: [tailwind(), preact()],
+  integrations: [tailwind({ applyBaseStyles: false })],
 });
