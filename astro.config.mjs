9fd0be00eb57912782a125ec05037bfbb3b98d25
yikZero <i@yikzero.com>
2024-05-08 14:48:06 +0800

    perf: remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
-import sitemap from "@astrojs/sitemap";
-
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://rosure.yikzero.com/',
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), preact()]
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
 });
